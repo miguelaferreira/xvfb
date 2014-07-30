e9abadc6674629cd0728fca2c6fdae32f69384eb
miguelaferreira <miguelferreira@me.com>
2014-07-30 16:39:03 +0200

    Make verison number standard

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vasily@locomote.com.au'
 license          'BSD'
 description      'Installs and configures xvfb as a service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3'
+version          '0.3.0'
 
 %w{centos ubuntu}.each do |os|
   supports os
